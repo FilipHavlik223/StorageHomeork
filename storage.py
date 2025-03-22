06d8c3b67715f9dcf2db235c501d32e5e4c4f82e
FilipHavlik223 <filiphavlik223@seznam.cz>
2025-03-23 00:13:54 +0100

    searching bar

--- storage.py
+++ storage.py
@@ -12,7 +12,7 @@ products = [
 
 def print_products():
     for product in products:
-        print(f"Název produktu2: {product['name']}, cena: {product['price']}$")
+        print(f"Název produktu2: {product['name']}, cena: {product['price']}Kč")
 
 
 def add_product():
@@ -25,13 +25,30 @@ def add_product():
 
     products.append(product2)
 
+def product_search():
+    key = input("Hledaný výrobek: ")
+    if len(key) == 0:
+        print("Na skladě není žádný výrobek")
+        menu()
+    query = []
+    for product in products:
+        if len(key) > len(product['name']):
+            continue
+        elif key.lower() == product['name'][0:len(key)].lower():
+            query.append(product)
+    print("Výsledek hledání: ")
+    for item in query:
+        print(f"{item['name']} - {item['price']} Kč")
+
 
 def menu():
     print("Vítej ve skladu")
     print("###############\n")
     print("1. Výpis položek")
-    print("2. Přidání položky\n")
-    print("3. Vyhledání položky\n")
+    print("2. Přidání položky")
+    print("3. Vyhledání položky")
+    print("4. Celková cena všech položek\n")
+
 
     choice = int(input("Volba: "))
 
@@ -49,10 +66,14 @@ def menu():
 
     elif choice == 3:
         print("Vyhledávání položky")
+        product_search()
+
 
     else:
         print("Zadal jsi špatně!\n")
         menu()
+
+
 
 
 menu()
